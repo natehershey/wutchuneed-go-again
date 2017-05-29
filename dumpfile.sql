0b003c941f5594c757fead4fa3783b2331ba3d8a
Nate Hershey <nate.hershey@welltok.com>
2017-05-29 09:42:24 -0600

    Implement index action

---
+++ dumpfile.sql
@@ -0,0 +1,97 @@
+-- mysqldump -u root -p --no-data dbname > schema.sql
+
+-- MySQL dump 10.13  Distrib 5.6.26, for osx10.10 (x86_64)
+--
+-- Host: localhost    Database: wutchuneed
+-- ------------------------------------------------------
+-- Server version	5.5.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `wutchuneed`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `wutchuneed` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `wutchuneed`;
+
+--
+-- Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `list_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `items`
+--
+
+DROP TABLE IF EXISTS `items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `measure` varchar(45) DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  `list_id` int(11) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `lists`
+--
+
+DROP TABLE IF EXISTS `lists`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `lists` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `list_type` varchar(45) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-05-29  9:40:54
--- main.go
+++ main.go
@@ -1,22 +1,74 @@
 package main
 
-import "fmt"
-import "net/http"
-import "github.com/gorilla/mux"
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	// "os"
+)
+
+var db *sql.DB
+var err error
+var foundLists = []List{}
+var foundCategories = []Category{}
+var foundItems = []Item{}
+
+type List struct {
+	Id         int        `json:"id"`
+	ListName   string     `json:"name"`
+	ListType   string     `json:"listType"`
+	Categories []Category `json:"categories"`
+}
+
+type Category struct {
+	Id           int    `json:"id"`
+	CategoryName string `json:"name"`
+	ListId       int    `json:"listId"`
+	Items        []Item `json:"items"`
+}
+
+type Item struct {
+	Id         int            `json:"id"`
+	ItemName   string         `json:"name"`
+	Quantity   int            `json:"quantity"`
+	Measure    sql.NullString `json:"measure"`
+	Status     sql.NullString `json:"status"`
+	CategoryId int            `json:"categoryId"`
+	ListId     int            `json:"listId"`
+}
 
 func main() {
+	getDbConnection()
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", HomeHandler)
 	r.HandleFunc("/lists", ListsHandler)
 	r.HandleFunc("/categories", CategoriesHandler)
 	r.HandleFunc("/items", ItemsHandler)
 	http.Handle("/", r)
-
-	fmt.Println(http.ListenAndServe(":8081", nil))
+	fmt.Println(http.ListenAndServe("localhost:8081", nil))
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Home")
+	fmt.Printf("Get lists\n")
+	allLists := getLists()
+	fmt.Printf("Found: %+v\n", allLists)
+
+	json, err := json.Marshal(allLists)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// os.Stdout.Write(json)
+	// fmt.Printf("ListsJson: %s\n", json)
+	// fmt.Print(w, json)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(json)
 }
 
 func ListsHandler(w http.ResponseWriter, r *http.Request) {
@@ -29,4 +81,99 @@ func CategoriesHandler(w http.ResponseWriter, r *http.Request) {
 
 func ItemsHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Items")
+}
+
+func getLists() []List {
+	fmt.Printf("'select id, name, list_type from lists;' in getLists()\n")
+	rows, err := db.Query("select id, name, list_type from lists;")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	fmt.Printf("select finished\n")
+
+	foundLists = []List{}
+
+	for rows.Next() {
+		var l List
+
+		fmt.Printf("Scanning rows\n")
+		err = rows.Scan(&l.Id, &l.ListName, &l.ListType)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("Finished scanning rows\n")
+
+		l.Categories = getCategoriesForList(l)
+
+		foundLists = append(foundLists, l)
+	}
+
+	return foundLists
+}
+
+func getCategoriesForList(l List) []Category {
+	fmt.Printf("getCategoriesForList(l List)\n")
+	rows, err := db.Query("select id, name from categories where list_id = ?;", l.Id)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	foundCategories = []Category{}
+
+	for rows.Next() {
+		var c Category
+
+		err = rows.Scan(&c.Id, &c.CategoryName)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		c.Items = getItemsForCategory(l, c)
+
+		foundCategories = append(foundCategories, c)
+		// fmt.Fprintf(w, "Name: %s, ID: %d, type: %s", l.name, l.id, l.listType)
+	}
+	return foundCategories
+}
+
+func getItemsForCategory(l List, c Category) []Item {
+	log.Print("select id, name, quantity, measure, status from items where category_id = %s;", c.Id)
+	rows, err := db.Query("SELECT id, name, quantity, measure, status FROM items WHERE list_id = ? AND category_id = ?;", l.Id, c.Id)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	fmt.Printf("got rows\n")
+	foundItems = []Item{}
+
+	for rows.Next() {
+		var i Item
+
+		fmt.Printf("Scanning into item\n")
+		err = rows.Scan(&i.Id, &i.ItemName, &i.Quantity, &i.Measure, &i.Status)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		foundItems = append(foundItems, i)
+		fmt.Printf("Name: %s, ID: %d, status: %s", i.ItemName, i.Id, i.Status)
+	}
+	return foundItems
+}
+
+func getDbConnection() {
+	db, err = sql.Open("mysql", "root@tcp(localhost:3306)/wutchuneed")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
 }
